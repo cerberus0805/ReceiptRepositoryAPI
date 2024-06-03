2287aa4a00a7f61e170334fcc034bbef1752c6e3
Ian Chang <cerberus0805@gmail.com>
2024-06-03 10:11:34 +0800

    extract application, config, router from main

---
+++ src/configuration.rs
@@ -0,0 +1,13 @@
+use std::error::Error;
+
+pub struct Config {
+    pub host: String,
+    pub port: u16
+}
+
+pub fn get_config() -> Result<Config, Box<dyn Error>> {
+    Ok(Config {
+        host: "0.0.0.0".to_string(),
+        port: 3000
+    })
+}
