e25f68d87046c10285a057b889447301ac00b4a9
Ian Chang <cerberus0805@gmail.com>
2024-07-19 00:52:19 +0800

    add product get api

--- src/models/v1/responses/response_product.rs
+++ src/models/v1/responses/response_product.rs
@@ -1,5 +1,7 @@
 use serde::Serialize;
 
+use crate::models::v1::errors::api_error::ApiError;
+
 #[derive(Serialize)]
 pub struct ResponseProduct {
     pub id: i32,
@@ -9,4 +11,17 @@ pub struct ResponseProduct {
     pub specification_unit: Option<String>,
     pub specification_others: Option<String>,
     pub brand: Option<String>
+}
+
+#[derive(Serialize)]
+pub struct ResponseProductPayload {
+    pub data: Option<ResponseProduct>,
+    pub error: Option<ApiError>
+}
+
+#[derive(Serialize)]
+pub struct ResponseProductsPayload {
+    pub data: Option<Vec<ResponseProduct>>,
+    pub total: Option<i64>,
+    pub error: Option<ApiError>
 }
