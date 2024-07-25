af1b30bd747f25fd0db8619781371f7597ecc5d6
Ian Chang <cerberus0805@gmail.com>
2024-07-25 20:42:40 +0800

    add data structure for inserting entity to database

--- src/models/v1/entities/entity_currency.rs
+++ src/models/v1/entities/entity_currency.rs
@@ -1,9 +1,16 @@
 use diesel::prelude::*;
 
-#[derive(Queryable, Selectable, Insertable, AsChangeset, Identifiable, Debug, PartialEq, Clone)]
+#[derive(Queryable, Selectable, AsChangeset, Identifiable, Debug, PartialEq, Clone)]
 #[diesel(table_name = crate::schema::currencies)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct EntityCurrency {
     pub id: i32,
+    pub name: String
+}
+
+#[derive(Insertable, Debug)]
+#[diesel(table_name = crate::schema::currencies)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct NewEntityCurrency {
     pub name: String
 }
--- src/models/v1/entities/entity_store.rs
+++ src/models/v1/entities/entity_store.rs
@@ -1,10 +1,20 @@
 use diesel::prelude::*;
 
-#[derive(Queryable, Selectable, Insertable, AsChangeset, Identifiable, Debug, PartialEq)]
+#[derive(Queryable, Selectable, AsChangeset, Identifiable, Debug, PartialEq)]
 #[diesel(table_name = crate::schema::stores)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct EntityStore {
     pub id: i32,
+    pub name: String,
+    pub alias: Option<String>,
+    pub branch: Option<String>,
+    pub address: Option<String>
+}
+
+#[derive(Insertable, Debug)]
+#[diesel(table_name = crate::schema::stores)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct NewEntityStore {
     pub name: String,
     pub alias: Option<String>,
     pub branch: Option<String>,
