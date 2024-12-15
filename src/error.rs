c9d044968c92071560a0ab888d0fd6e39468bbc3
Ian Chang <cerberus0805@gmail.com>
2024-12-15 16:05:44 +0800

    add error into response

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,23 @@
+use http::StatusCode;
+use axum::response::{IntoResponse, Response};
+
 pub type Result<T> = core::result::Result<T, Error>;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Error {
-    ConfigMissingEnv(&'static str)
+    ConfigMissingEnv(&'static str),
+    LoginFailed
+}
+
+impl IntoResponse for Error {
+    fn into_response(self) -> Response {
+        match self {
+            Error::LoginFailed => {
+                (StatusCode::UNAUTHORIZED, "LOGIN_FAILED").into_response()
+            },
+            _ => {
+                (StatusCode::INTERNAL_SERVER_ERROR, "GENERIC_ERROR").into_response()
+            }
+        }
+    }
 }
